3efbc4c40b7454914f9b4e5ed7e97c11ea6436c4
admin <admin@YD2-09>
2022-06-13 15:18:01 +0900

    다시

---
+++ Aproject/src/main/java/co/prj/Aproject/member/command/LoginCommand.java
@@ -0,0 +1,16 @@
+package co.prj.Aproject.member.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.prj.Aproject.comm.Command;
+
+public class LoginCommand implements Command {
+
+	@Override
+	public String exec(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return "login";
+	}
+
+}
