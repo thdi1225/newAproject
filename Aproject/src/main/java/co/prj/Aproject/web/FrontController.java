c7a736304f05f6c135d7caa0defe3ed35101b85c
admin <admin@YD2-09>
2022-06-13 14:53:26 +0900

    test

---
+++ Aproject/src/main/java/co/prj/Aproject/web/FrontController.java
@@ -0,0 +1,47 @@
+package co.prj.Aproject.web;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.prj.Aproject.comm.Command;
+import co.prj.Aproject.home.command.HomeCommand;
+
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private HashMap<String, Command> map = new HashMap<String, Command>();
+
+	public FrontController() {
+		super();
+	}
+
+	public void init(ServletConfig config) throws ServletException {
+		map.put("/home.do", new HomeCommand());
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String uri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String page = uri.substring(contextPath.length());
+
+		Command command = map.get(page);
+		String viewPage = command.exec(request, response);
+
+		if (!viewPage.endsWith(".do") && !viewPage.equals(null)) {
+			viewPage = viewPage + ".tiles";
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+
+}
